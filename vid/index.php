fc02c8667bddf3df6113c8ee805af6a6d45fa500
Constant Pagoui <constantpagoui@gmail.com>
2017-08-30 00:16:27 -0400

    Create index.php

---
+++ vid/index.php
@@ -0,0 +1,125 @@
+<?php include_once("../mon_prenom.php");
+	// Include database connection and functions here.  See 3.1. 
+	include_once("../db_management/DBConnector.php");
+	include_once("../db_management/login_functions.php");
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title><?php echo nom_du_site; ?></title>
+<link rel="stylesheet" type="text/css" href="design/assets/css/main.css" />
+<link rel="stylesheet" type="text/css" href="../design/assets/css/main.css" />
+<link rel="stylesheet" type="text/css" href="../design/bootstrap/css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="../design/bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet" href="../design/bootstrap/css/bootstrap-theme.css" />
+<link rel="stylesheet" href="../design/bootstrap/css/bootstrap-theme.min.css" />
+<link rel="stylesheet" href="themes/maccaco/projekktor.style.css" type="text/css" media="screen" />
+<script src="../design/js/jquery-3.1.1"></script>
+<script type="text/JavaScript" src="../design/js/sha512.js"></script> 
+<script type="text/JavaScript" src="../design/js/forms.js"></script> 
+<script type="text/javascript" src="design/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="design/projekktor-1.3.09.min.js"></script>
+<style>
+
+video::-internal-media-controls-download-button {
+    display:none;
+}
+
+video::-webkit-media-controls-enclosure {
+    overflow:hidden;
+}
+
+video::-webkit-media-controls-panel {
+    width: calc(100% + 30px); /* Adjust as needed */
+}
+</style>
+<script type="text/javascript">
+         $(document).ready(function() {
+               pprojekktor('#player_a', {
+				volume: 0.8,
+				playerFlashMP4: 'StrobeMediaPlayback/StrobeMediaPlayback.swf',
+				playerFlashMP3: 'StrobeMediaPlayback/StrobeMediaPlayback.swf'
+    });
+});
+
+	function readInput(str)
+            {
+                 if(str!= ''){
+                    $('#search_result').html(' ');
+                    $.ajax(
+                    {
+						url:"search_manager.php",
+                        method: "post",
+                        data:{searchthis:str},
+                        dataType:"text",
+                        success:function(data)
+                        {
+                            $('#search_result').html(data)
+                        }
+                    });
+                 }
+                 else{
+                     $('#search_result').html(' ');
+                }
+				//function that detects when the user hits enter
+				if_user_hits_enter('keyword');
+            }
+          
+			function call_my_eagl(str)
+            {
+                if(str!= ''){
+                    $.ajax(
+                    {
+                        url:"search_manager.php",
+                        method: "post",
+                        data:{search_doc:str},
+                        dataType:"text",
+                        success:function(data)
+                        {
+							 $('#old_search').html('')
+                             $('#disp_default').html('')
+                             $('#disp_result').html(data)
+                             $('#keyword').html(str)
+                             
+                        }
+                    });
+                 }
+				 if_user_hits_enter('keyword');
+            }
+			
+			
+			function if_user_hits_enter(get_input_id)
+			{
+				document.getElementById(get_input_id).onkeypress = function(e){
+					if (!e) e = window.event;
+					var keyCode = e.keyCode || e.which;
+					if (keyCode == '13'){
+					  // Enter pressed
+					  call_my_eagl(this.value);
+					  $('#search_result').html('');
+					  return true;
+					}
+				}
+				//get_file_type(file_type)
+			}
+			
+				//function that detects when the user hits enter
+				if_user_hits_enter('keyword');
+    </script>
+</head>
+
+<body>
+	<?php
+		//if(!isset($_SESSION['user_id'], $_SERVER['login_string']))
+			//header('Location: ../acceuil');
+		//else
+			include("control/controller.php");
+	?>
+<!--div class="page_content" id="p_content">
+
+       <a href="files/pdf/Pagoui.pdf"> <p>It appears your web browser doesn't support iframes.</p></a>
+   
+</div-->
+</body>
