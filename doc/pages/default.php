5f9fb77587f714890aa80870e03bfa5541af26eb
Constant Pagoui <constantpagoui@gmail.com>
2017-08-30 00:11:21 -0400

    Add files via upload

---
+++ doc/pages/default.php
@@ -0,0 +1,42 @@
+ <script>
+ 	function call_eag(){
+				dat = document.getElementById("keyword").value;
+				call_my_eagl(dat);
+				//alert(dat);
+			}
+	
+</script>
+ <div id="old_search">
+     <div class='search_area_searching_page' >
+                        <div class='search_bk'><?php echo nom_du_site; ?></div>
+                        
+                        <div class="input-group">
+                            <input class="form-control input-lg" name="keyword" id="keyword" onkeyup="readInput(this.value)" type="text" placeholder="Rechercher un document">
+                            <span class="input-group-addon" onClick="call_eag()" style="cursor:pointer;">Rechercher</span>
+                        </div>
+                        <div id="search_result" class="point"></div>
+                </div>
+        
+    <div class="b_cont" id="disp_default">
+            
+            <?php
+                $conn = new DBConnector();
+                $con = $conn->makeTheConnection(); 
+                $my_query = "SELECT DISTINCT * FROM dwd_all_file_info WHERE dwd_afi_file_type IN ('pdf','docx','xlsx', 'ppt', 'pptx', 'txt')  ORDER BY dwd_afi_file_num_of_downloads DESC LIMIT 18";
+                $my_result = mysqli_query($con, $my_query)or die(mysqli_error($con));;
+                if(mysqli_num_rows($my_result) >0 ){ $i = 0;
+                while($result_line = mysqli_fetch_array($my_result)){
+                    if(($i+1) % 6 ==0)
+                    echo '<div class="row">';
+                  echo'<div class="col-xs-2 col-md-2">
+                       
+                            <span class="glyphicon glyphicon-book" aria-hidden="true" style="font-size:5vw;cursor:pointer; color:#676565;"></span><br>                       <a style="font-weight:normal; font-size:1vw;cursor:pointer;" class="wordwrap"> '.$result_line['dwd_afi_file_name'].'</a>
+                    </div>
+                  '; 
+                  if(($i+1) % 6 ==0)
+                    echo '</div>';$i++;
+                        }
+                  }?>
+            </div>
+          </div>
+      <div id="disp_result"></div>
---
+++ doc/pages/present_book.php
@@ -0,0 +1,135 @@
+
+    <div class='search_area_searching_page'>
+                    <div class='search_bk'><?php echo nom_du_site; ?></div>
+                    <input class="input" name="keyword" id="keyword" onkeyup="readInput(this.value)" type="text" placeholder="Rechercher un document similaire" />
+                    <div id="search_result" class="point"></div>
+            </div>
+            
+<?php 
+	$conn = new DBConnector();
+	$conn->makeTheConnection();
+	$text = $conn->get_s_suggestions(substr($file_name, 0, 3));
+?>
+<div class="b_cont">
+	<!--Present the book to th euser-->
+        <!--div class="col-xs-4">
+        	<div class="liv_sim"><h5>Documents similaires</h5></div><br>
+            
+            <!--div style="font-size:4em;">
+          		<span class="glyphicon glyphicon-book" aria-hidden="true"></span>
+            </div-->
+            <!--div class="row">
+              <div class="col-xs-6 col-md-3">
+                     <div style="font-size:4em;">
+                        <span class="glyphicon glyphicon-book" aria-hidden="true"></span>
+                    </div>
+                   <a> <php echo $text[0];?></a>
+                </div>
+              <div class="col-xs-6 col-md-3">
+                     <div style="font-size:4em;">
+                        <span class="glyphicon glyphicon-book" aria-hidden="true"></span>
+                    </div>
+                   <a> <php echo $text[1];?></a>
+                </div>
+              <div class="col-xs-6 col-md-3">
+                     <div style="font-size:4em;">
+                        <span class="glyphicon glyphicon-book" aria-hidden="true"></span>
+                    </div>
+                   <a href="index.php?f=3"> <php echo $text[2];?></a>
+                </div>
+            </div>
+            <div class="row">
+              <div class="col-xs-6 col-md-3">
+                <div style="font-size:4em;">
+          			<span class="glyphicon glyphicon-book" aria-hidden="true"></span>
+            	</div>
+                <a href="index.php?f=4"><php echo $text[3];?></a>
+              </div>
+              <div class="col-xs-6 col-md-3">
+                <div style="font-size:4em;color:#676565;">
+          			<span class="glyphicon glyphicon-book" aria-hidden="true"></span>
+            	</div>
+               <a>ihbuh</a>
+              </div>
+            </div>
+        </div>
+        <div class="col-xs-8"><div class="wordwrap"-->
+            
+                <?php 
+				$f_path = substr(realpath(dirname(__FILE__)), 0, strlen(realpath(dirname(__FILE__)))-5);
+				echo '<h2 id="office-presentation-odp-">Nom du document</h2>
+				<iframe src = "ViewerJS/#../files/education/myfile.pdf" width="800vw" height="550vw" allowfullscreen webkitallowfullscreen></iframe>';
+					/*echo  "<object data='".$f_path."'/files/education/myfile.pdf' type='application/pdf' style='width:70%; height:40em;'>
+					<embed src='".$f_path."'/files/education/myfile.pdf' type='application/pdf' style='width:70%; height:40em'></embed>
+					</object>";*/
+				 ?>
+                
+                <!--div class="book_text"><--?php echo $file_name; ?></div>
+                <div class="book_desc"><br><--?php echo $the_keys; ?></div>
+            
+        </div></div>
+    	<div class="row"><div class="col-xs-4"><div class="liv_sim"><br>
+        <h5>Description du document</h5><div class="wordwrap"><php echo $the_keys; ?>
+        </div></div>
+        <div class="row"><div class="col-xs-4"><div class="liv_sim"><br><br>
+        <button class="btn" value="telecharger">Telecharger le document</button>
+        <button class="btn" value="recommander">Recommander le document</button>
+        <button class="btn" value="ouvrir">Ouvrir le document</button></div>
+    <!--Recommand similar books to the user-->
+</div>
+<script>
+function readInput(str)
+            {
+                 if(str!= ''){
+                    $('#search_result').html(' ');
+                    $.ajax(
+                    {
+						url:"temp_page.php",
+                        method: "post",
+                        data:{searchthis:str},
+                        dataType:"text",
+                        success:function(data)
+                        {
+                            $('#search_result').html(data)
+                        }
+                    });
+                 }
+                 else{
+                     $('#search_result').html(' ');
+                }
+				//function that detects when the user hits enter
+				if_user_hits_enter('keyword');
+            }
+			
+            //When the user hits enter
+            function define_style(elt)
+            {
+					document.getElementById(elt).style.color = "black";
+                	document.getElementById(elt).style.fontWeight = "bold";
+            }
+			
+			function call_my_eagl(str, ty)
+            {
+                if(str!= ''){
+                    $.ajax(
+                    {
+                        url:"temp_page.php",
+                        method: "post",
+                        data:{p_page:str, t:ty},
+                        dataType:"text",
+                        success:function(data)
+                        {
+                             $('#home_search').html('')
+                             $('#page_content').html(data)
+                             $('#search_input').html(str)
+                             $('#search_result').html('')
+                        }
+                    });
+                 }
+				 if_user_hits_enter('search_input');
+            }
+			
+</script>
+<?php
+	
+?>
